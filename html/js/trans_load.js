af869f44c340aa2dc5358c62399a9850a98aec85
Kishor SS <kishor.sathigrama@gmail.com>
2016-11-21 16:15:49 +0530

    commit -1 Devagitam old website code

---
+++ html/js/trans_load.js
@@ -0,0 +1,26 @@
+ // Load the Google Transliterate API
+      google.load("elements", "1", {
+            packages: "transliteration"
+          });
+
+      function onLoad() {
+        var options = {
+          sourceLanguage: 'en', // or google.elements.transliteration.LanguageCode.ENGLISH,
+          destinationLanguage: ['kn'], // or [google.elements.transliteration.LanguageCode.HINDI],
+          shortcutKey: 'ctrl+g',
+          transliterationEnabled: true
+        };
+        // Create an instance on TransliterationControl with the required
+        // options.
+        var control =
+            new google.elements.transliteration.TransliterationControl(options);
+
+        // Enable transliteration in the textfields with the given ids.
+        var ids = [ "com_text" ];
+        control.makeTransliteratable(ids);
+
+        // Show the transliteration control which can be used to toggle between
+        // English and Hindi.
+        control.showControl('translControl');
+      }
+      google.setOnLoadCallback(onLoad);
